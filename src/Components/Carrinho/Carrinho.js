b8175621c04e998a27be1124094c79ec91b5bfab
mibrito1 <mirian.unasp@gmail.com>
2023-05-18 22:17:12 -0300

    criei os componente principais do projeto

---
+++ src/Components/Carrinho/Carrinho.js
@@ -0,0 +1,11 @@
+
+import React from "react";
+export function Carrinho(){
+    return(
+        <div>
+            <p>
+                Aqui vi o carrinho de compras
+            </p>
+        </div>
+    )
+}
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+
+
+export function Home(){
+    return(
+        <div>
+            <p>
+                Aqui vai o home
+            </p>
+        </div>
+    )
+}
